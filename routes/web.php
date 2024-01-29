029bbfadeec54884a76bfbbb4a54d3e5917ac9ed
MansoorAli6266 <mansooralikhn252@gmail.com>
2024-01-29 12:05:01 -0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ManageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/',[ManageController::class,'index'])->name('index');
+
+
